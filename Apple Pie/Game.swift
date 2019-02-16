eb4a6dfde1f1359fc927f23b2586cbdb5eb9214e
Buffy Feinstein <buffyfeinstein@gmail.com>
2019-02-15 22:03:06 -0600

    add image and layout  and code for new round for each game

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Barbara Feinstein on 2/15/19.
+//  Copyright © 2019 Barbara Feinstein. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var listOfWords = ["jump", "run", "swim", "cycle", "hike"]
 
+let incorrectMovesAllowed = 7
+
+var totalWins = 0
+var totalLosses = 0
+
+class ViewController: UIViewController {
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       newRound()
+        
     }
+    var currentGame: Game!
 
-
+    func newRound(){
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI() {
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
 }
 
